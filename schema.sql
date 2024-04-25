0a86e3a407eb6373d3da83a5b56077125dc79edd
Jonas Strassel <info@jonas-strassel.de>
2024-04-25 09:45:00 +0200

    🌱 Initial version of the service (only update)

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS packages;
+CREATE TABLE packages (
+    name TEXT NOT NULL,
+    arch TEXT NOT NULL,
+    branch TEXT NOT NULL,
+    repo TEXT NOT NULL,
+    version TEXT NOT NULL,
+    description TEXT NOT NULL,
+    builddate INTEGER NOT NULL,
+    PRIMARY KEY (`name`, `arch`, `branch`, `repo`)
+);
